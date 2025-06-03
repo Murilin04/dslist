e69dc9bdf6ea4c404aa5544c4bbc9e1cdb452645
Murilo Barros <barrosmurilo85@gmail.com>
2025-06-03 00:31:47 -0300

    feat: Adjust asset configuration in angular.json and fix route rendering with router-outlet

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { GamesComponent } from './pages/games/games.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'games', component: GamesComponent },
+];
