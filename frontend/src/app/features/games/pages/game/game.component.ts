afdf98dd98f6fb310efef6de2494a7250c9cd8d7
Murilo Barros <barrosmurilo85@gmail.com>
2025-06-26 18:29:33 -0300

    feat: added new background images, full game descriptions, CSS tweaks and star rating using database score

--- frontend/src/app/features/games/pages/game/game.component.ts
+++ frontend/src/app/features/games/pages/game/game.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { GameService } from '../../services/game.service';
 import { Game } from '../../models/game.model';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-game',
-  imports: [ CommonModule, RouterModule],
+  imports: [ CommonModule, RouterModule, MatIconModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
@@ -26,6 +27,21 @@ export class GameComponent {
         this.games = [game];
       });
     }
+
+  }
+
+  getStars(score: number): ('completa' | 'metade' | 'vazia')[] {
+
+    const estrela: ('completa' | 'metade' | 'vazia')[] = [];
+    const estrelaCompleta = Math.floor(score);
+    const meiaEstrela = score % 1 >= 0.25 && score % 1 < 0.75;
+    const estrelaVazia = 5 - estrelaCompleta - (meiaEstrela ? 1 : 0);
+
+    for (let i = 0; i < estrelaCompleta; i++) estrela.push('completa');
+    if (meiaEstrela) estrela.push('metade');
+    for (let i = 0; i < estrelaVazia; i++) estrela.push('vazia');
+
+    return estrela;
 
   }
 
